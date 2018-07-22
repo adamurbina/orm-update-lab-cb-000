1e4edc999db67baa3431771a9bf9b5a0b9eb3eea
Adam Urbina <adamurbina@gmail.com>
2018-07-22 19:52:32 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,15 @@ class Student
         @grade = grade
     end
 
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXIST students(
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                grade TEXT
+            )
+        SQL
+    end
 
 
 end
