1dd4483504f31e5a8fa642fcd61e719f085af380
Adam Urbina <adamurbina@gmail.com>
2018-07-22 20:15:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,5 +55,10 @@ class Student
         student
     end
 
-
+    def update
+        sql = <<-SQL
+            UPDATE students SET name = ?, grade = ? WHERE id = ?
+        SQL
+        DB[:conn].execute(sql, self.name, self.grade, self.id)
+    end
 end
